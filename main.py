e0f13d17c9c84054ad46cb1422d667c90563e547
eddie0101 <ghiuzaneduard@gmail.com>
2025-09-11 10:11:26 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from turtle import Screen, Turtle
+import time
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("snake game")
+screen.tracer(False)
+
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+screen.update()
+
+snake.move()
+
+
+screen.exitonclick()

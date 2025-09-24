f9d8cfec3ceff406c1d61acaeea38667cc39f890
Eduard <42815296+eddie0101@users.noreply.github.com>
2025-09-24 09:46:32 +0000

    Added tail collision and gameover screen

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
 # Shared constants for the Snake game
 MOVE_DISTANCE = 20
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0), (-60, 0), (-80, 0)]
+SNAKE_DEFAULT_SPEED = 200 # lower is faster
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from snake import Snake
 from food import Food
+import constants
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -25,6 +26,22 @@ def check_food_collision():
     if abs(x_s - x_f) < 0.1 and abs(y_s - y_f) < 0.1:
         return True
 
+def check_tail_collision():
+    x_s, y_s = snake.get_head_coords()
+    for segment in snake.segments[1:]:
+        x_seg, y_seg = segment.position()
+        if abs(x_s - x_seg) < 0.1 and abs(y_s - y_seg) < 0.1:
+            return True
+    return False
+
+def gameover():
+    screen.clear()
+    t = Turtle()
+    t.penup()
+    t.goto(0, 0)  # Position in the center
+    t.write("GAME OVER!", align="center", font=("Arial", 24, "bold"))
+    t.hideturtle()
+
 def debug():
     print(f"Snake head coords: {snake.get_head_coords()}")
     print(f"Food coords: {food.get_coords()}")
@@ -34,9 +51,11 @@ def game_run():
     if check_food_collision(): 
         food.next_cookie()
         snake.grow()
+    if check_tail_collision():
+        gameover()
     screen.update()
     debug()
-    screen.ontimer(game_run, 100)
+    screen.ontimer(game_run, constants.SNAKE_DEFAULT_SPEED)
 
 game_run()
 

705257722962c6ff783ad17e8fbd8a966d71aab6
Eduard <42815296+eddie0101@users.noreply.github.com>
2025-11-13 06:07:54 +0000

    PyLint fixes in food.py file

--- food.py
+++ food.py
@@ -6,20 +6,23 @@ FOOD_GEN_BOUNDARY = 280
 FOOD_SIZE = (int)(FOOD_GEN_BOUNDARY / MOVE_DISTANCE) # food generation boundary
 
 class Food:
+    """
+    This class is responsible for creating and managing the food that the snake eats.
+    """
 
-  def __init__(self):
-    self.food = Turtle("circle")
-    self.create_food()
+    def __init__(self):
+        self.food = Turtle("circle")
+        self.create_food()
 
-  def create_food(self):
-    self.food.color("black") # fill color
-    self.food.penup()
-    self.food.pencolor("yellow") # outline color
-    self.food.shapesize(outline=2)
-    self.next_cookie()
+    def create_food(self):
+        self.food.color("black") # fill color
+        self.food.penup()
+        self.food.pencolor("yellow") # outline color
+        self.food.shapesize(outline=2)
+        self.next_cookie()
 
-  def next_cookie(self):
-    self.food.goto(random.randint(-FOOD_SIZE, FOOD_SIZE) * MOVE_DISTANCE, random.randint(-FOOD_SIZE, FOOD_SIZE) * MOVE_DISTANCE) # move food to random location\
+    def next_cookie(self):
+        self.food.goto(random.randint(-FOOD_SIZE, FOOD_SIZE) * MOVE_DISTANCE, random.randint(-FOOD_SIZE, FOOD_SIZE) * MOVE_DISTANCE) # move food to random location\
 
-  def get_coords(self):
-    return self.food.position()
+    def get_coords(self):
+        return self.food.position()

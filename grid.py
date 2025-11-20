1c6b51e6b11fb4c573b36a43418cd49dcd6e4ae0
Eduard <42815296+eddie0101@users.noreply.github.com>
2025-11-20 12:17:23 +0000

    Complete and better grid class by fixing some issues
    Fixed the initial coordinates that gave the base for constructing the vertical and horizontal grid lines. Also finished grid implementation of horizontal grid lines

--- grid.py
+++ grid.py
@@ -11,15 +11,28 @@ class Grid(Turtle):
 
     def create_grid(self):
         self.color("white")
+        self.pensize(1)
 
+        # coordinates for the first vertical grid line
         x1_coord = SCREEN_WIDTH / -2
-        y1_coord = SCREEN_LENGTH / -2
-        x2_coord = SCREEN_WIDTH / 2
-        y2_coord = SCREEN_LENGTH / 2
+        y1_coord = SCREEN_LENGTH / 2
+        x2_coord = SCREEN_WIDTH / -2
+        y2_coord = SCREEN_LENGTH / -2
 
         for i in range(SCREEN_WIDTH // CELL_SIZE):
             self.penup()
-            self.goto(x1_coord + i, y1_coord)
+            self.goto(x1_coord + i * CELL_SIZE, y1_coord)
             self.pendown()
-            self.goto(x2_coord + i, y2_coord)
+            self.goto(x2_coord + i * CELL_SIZE, y2_coord)
 
+        # coordinates for the first horizontal grid line
+        x1_coord =  SCREEN_WIDTH / -2
+        y1_coord = SCREEN_LENGTH / 2
+        x2_coord = SCREEN_WIDTH / 2
+        y2_coord = SCREEN_LENGTH / 2
+
+        for i in range(SCREEN_LENGTH // CELL_SIZE):
+            self.penup()
+            self.goto(x1_coord, y1_coord - i * CELL_SIZE)
+            self.pendown()
+            self.goto(x2_coord, y2_coord - i * CELL_SIZE)
